8fb7a8c9812d028cdaf20425c859742940f2e3a9
Ritesh Rao V <ritheshraov016@gmail.com>
2024-08-10 20:49:10 +0530

    added views to get product by brand and category

--- brands_and_categories/urls.py
+++ brands_and_categories/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from brands_and_categories.views import BrandsRetrieveUpdateDestroyView, BrandsView, CategoriesRetrieveUpdateDestroyView, CategoriesView
+from brands_and_categories.views import BrandProductsView, BrandsRetrieveUpdateDestroyView, BrandsView, CategoriesRetrieveUpdateDestroyView, CategoriesView, CategoryProductsView
 
 
 urlpatterns = [
     path('brand/', BrandsView.as_view()),
     path('brand/<int:pk>', BrandsRetrieveUpdateDestroyView.as_view()),
+    path('brand/products/<int:brand_id>',BrandProductsView.as_view()),
     path('category/', CategoriesView.as_view()),
-    path('category/<int:pk>', CategoriesRetrieveUpdateDestroyView.as_view())
+    path('category/<int:pk>', CategoriesRetrieveUpdateDestroyView.as_view()),
+    path('category/products/<int:category_id>',CategoryProductsView.as_view()),
 ]
--- brands_and_categories/views.py
+++ brands_and_categories/views.py
@@ -5,6 +5,9 @@ from brands_and_categories.models import Brand, Category
 from brands_and_categories.serializers import BrandSerializer, CategorySerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 
+from shop.models import Product
+from shop.serializers import ProductSerializer
+
 
 # Create your views here.
 class BrandsView(generics.ListCreateAPIView):
@@ -26,3 +29,19 @@ class CategoriesView(generics.ListCreateAPIView):
 class CategoriesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class BrandProductsView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        brand_id=self.kwargs.get('brand_id')
+        return Product.objects.filter(brand=brand_id)
+    
+class CategoryProductsView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        category_id=self.kwargs.get('category_id')
+        return Product.objects.filter(category=category_id)
